e01c4da6db86fbcbb302ad2685fa821ec2ec0ab3
Robert Auer <robert.auer@cloudogu.com>
2017-08-07 12:10:49 +0200

    Add post-upgrade script which automatically starts db migration

---
+++ resources/post-upgrade.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+echo "wait until sonar passes all health checks"
+if ! doguctl healthy --wait --timeout 120 sonar; then
+  echo "timeout reached by waiting of sonar to get healthy"
+  exit 1
+fi
+curl --insecure -X POST https://$(doguctl config --global fqdn)/sonar/api/system/migrate_db
