3cd631dbc9017413eb43c618f24d5565a6418bde
Maren Süwer <maren.suewer@triology.de>
2018-04-16 10:24:02 +0200

    for loop to import all quality profile files

--- resources/qualityProfilesImport.sh
+++ resources/qualityProfilesImport.sh
@@ -8,5 +8,9 @@ if ! doguctl healthy --wait --timeout 120 sonar; then
   echo "timeout reached by waiting of sonar to get healthy"
   exit 1
 fi
-curl --insecure -X POST -u admin:adminpw -F 'backup=@opt/sonar/qualityprofiles/java-sonar-way-92069.xml' -v https://$(doguctl config --global fqdn)/sonar/api/qualityprofiles/restore 
-echo "import of quality profiles successful"
+
+for file in /opt/sonar/qualityprofiles/*
+do
+  curl --insecure -X POST -u admin:adminpw -F "backup=@$file" -v https://$(doguctl config --global fqdn)/sonar/api/qualityprofiles/restore
+  echo "import of quality profile $file successful"
+done;
