121a8625943ba89eaf3e709ffa82deca4c368035
Anupam Mukherjee <amukherjee@triobs1.local>
2018-04-24 09:56:18 +0200

    Changed user details in Config to suit the general tests

--- integrationTests/config.js
+++ integrationTests/config.js
@@ -1,7 +1,7 @@
 let cesFqdn = process.env.CES_FQDN;
 if (!cesFqdn) {
   // url from ecosystem with private network
-  cesFqdn = "192.168.56.2" //"192.168.42.2"
+  cesFqdn = "192.168.42.2"
 }
 
 let webdriverType = process.env.WEBDRIVER;
@@ -13,14 +13,14 @@ module.exports = {
     fqdn: cesFqdn,
     baseUrl: 'https://' + cesFqdn,
     sonarContextPath: '/sonar',
-    username: 'amukherjee',//'ces-admin',
-    password: 'admin',//'ecosystem2016',
+    username: 'ces-admin',
+    password: 'ecosystem2016',
     firstname:'admin',
     lastname: 'admin',
-    displayName: 'amukherjee',//'ces-admin',
+    displayName: 'ces-admin',
     email: 'ces-admin@cloudogu.com',
     webdriverType: webdriverType,
     debug: true,
-    adminGroup: 'ecosystemAdmins',//'CesAdministrators',
+    adminGroup: 'CesAdministrators',
 	sonarqubeToken: 'sonarqubeToken'
 };
