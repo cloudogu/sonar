255940049a1b6916c5177a3d6f8c1f99e16b48a9
Robert Auer <robert.auer@cloudogu.com>
2019-03-06 11:49:56 +0100

    Deactivate default admin account via REST API instead of database call; #5

--- resources/util.sh
+++ resources/util.sh
@@ -85,7 +85,7 @@ function create_dogu_admin_user_and_save_password() {
   echo "Creating ${DOGU_ADMIN} and granting admin permissions..."
   DOGU_ADMIN_PASSWORD=$(doguctl random)
   create_user_via_rest_api "${DOGU_ADMIN}" "SonarQubeDoguAdmin" "${DOGU_ADMIN_PASSWORD}" "${AUTH_USER}" "${AUTH_PASSWORD}" "${LOG_LEVEL}"
-  add_user_to_group_via_rest_api "${DOGU_ADMIN}" sonar-administrators "${AUTH_USER}" "${AUTH_PASSWORD}" "${LOG_LEVEL}"
+  add_user_to_group_via_rest_api "${DOGU_ADMIN}" "sonar-administrators" "${AUTH_USER}" "${AUTH_PASSWORD}" "${LOG_LEVEL}"
   # saving dogu admin password in registry
   doguctl config -e dogu_admin_password "${DOGU_ADMIN_PASSWORD}"
   printf "\\n"
@@ -111,13 +111,24 @@ function add_user_to_group_via_rest_api() {
   curl "${LOG_LEVEL}" --fail -u "${AUTH_USER}":"${AUTH_PASSWORD}" -X POST "http://localhost:9000/sonar/api/user_groups/add_user?name=${GROUPNAME}&login=${USERNAME}"
 }
 
+function deactivate_default_admin_user() {
+  AUTH_USER=$1
+  AUTH_PASSWORD=$2
+  LOG_LEVEL=$3
+  RANDOM_PASSWORD=$(doguctl random)
+  curl "${LOG_LEVEL}" --fail -u "${AUTH_USER}":"${AUTH_PASSWORD}" -X POST "http://localhost:9000/sonar/api/users/change_password?login=admin&password=${RANDOM_PASSWORD}&previousPassword=admin"
+  curl "${LOG_LEVEL}" --fail -u "${AUTH_USER}":"${AUTH_PASSWORD}" -X POST "http://localhost:9000/sonar/api/users/deactivate?login=admin"
+}
+
 function run_first_start_and_post_upgrade_tasks() {
   LOG_LEVEL=$1
   # default admin credentials (admin, admin) are used
   create_dogu_admin_user_and_save_password admin admin "${LOG_LEVEL}"
 
-  echo "Removing default admin account..."
-  execute_sql_statement_on_database "DELETE FROM users WHERE login='admin';"
+  echo "Deactivating default admin account..."
+  DOGU_ADMIN_PASSWORD=$(doguctl config -e dogu_admin_password)
+  deactivate_default_admin_user ${DOGU_ADMIN} "${DOGU_ADMIN_PASSWORD}" "${LOG_LEVEL}"
+  printf "\\n"
 
   echo "Waiting for configuration changes to be internally executed..."
   sleep 3
