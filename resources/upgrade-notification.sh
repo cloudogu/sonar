377d69e12f5fc16a2233217c21e23a47a0366095
Robert Auer <robert.auer@cloudogu.com>
2019-03-07 10:10:38 +0100

    Add plugin upgrade notification to upgrade-notification script; #5

--- resources/upgrade-notification.sh
+++ resources/upgrade-notification.sh
@@ -13,4 +13,5 @@ if [[ ${FROM_VERSION} == *"5.6.6"* ]] && [[ ${TO_VERSION} == *"6.7."* ]]; then
   exit 1
 elif [[ ${FROM_VERSION} == *"5.6.7"* ]] && [[ ${TO_VERSION} == *"6.7."* ]]; then
   echo "You are upgrading your SonarQube instance from 5.6.7 to 6.7.x LTS. Please consider backing up your SonarQube database. Upgrade problems are rare, but you'll want the backup if anything does happen."
+  echo "If you are using plugins which are not up-to-date, they will be updated during the upgrade process."
 fi
