e3f46a3789146ea5763c3289bfa9e09711e313bc
Robert Auer <robert.auer@cloudogu.com>
2019-12-16 15:04:40 +0100

    Make sure vm.max_map_count is high enough before upgrade

--- resources/upgrade-notification.sh
+++ resources/upgrade-notification.sh
@@ -10,3 +10,8 @@ if [[ ${FROM_VERSION} == *"5"* ]]; then
   echo "Upgrade from version ${FROM_VERSION} to ${TO_VERSION} is not supported. Upgrade to version 6.7.7-2 before."
   exit 1
 fi
+
+if [[ $(cat /proc/sys/vm/max_map_count) -lt 262144 ]]; then
+  echo "Your max virtual memory areas vm.max_map_count is too low, increase to at least [262144]. You can do so by upgrading the ces-commons package to at least version 0.2.0."
+  exit 1
+fi
