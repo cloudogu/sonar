8ea1b2260169cf9f988bca67376cd47e0fdd1a91
Robert Auer <robert.auer@cloudogu.com>
2021-11-15 11:53:55 +0100

    Wait until logouthandler is injected before logging out; #67

--- integrationTests/cypress/support/step_definitions/required_steps_for_dogu_lib.js
+++ integrationTests/cypress/support/step_definitions/required_steps_for_dogu_lib.js
@@ -5,6 +5,9 @@ doguTestLibrary.registerSteps()
 
 When(/^the user clicks the dogu logout button$/, function () {
     cy.visit("/" + env.GetDoguName(), { failOnStatusCode: false })
+    // wait until the logoutMenuHandler is injected
+    // see https://github.com/cloudogu/sonar-cas-plugin/blob/develop/src/main/java/org/sonar/plugins/cas/logout/CasSonarSignOutInjectorFilter.java#L132
+    cy.wait(1000)
     // Click user menu button
     cy.get('*[class^="dropdown-toggle navbar-avatar"]').scrollIntoView().click();
     // Click logout button
