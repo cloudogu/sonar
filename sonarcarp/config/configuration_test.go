0ca4fcf7229885c0715ea9af9957fc41cda5f6e8
Philipp Pixel <boris.schaa@cloudogu.com>
2025-08-22 09:28:21 +0200

    #136  Replace grafana copypasta

--- sonarcarp/config/configuration_test.go
+++ sonarcarp/config/configuration_test.go
@@ -11,30 +11,30 @@ import (
 const templateConfig = `
 base-url: https://localhost:8080
 cas-url: https://192.168.56.2/cas
-service-url: https://localhost:8080/grafana/login
-target-url: http://localhost:3000/grafana/login
+service-url: https://localhost:8080/sonar/login
+target-url: http://localhost:3000/sonar/login
 logout-method: GET
-logout-path: \/grafana\/(cas\/)?logout
+logout-path: \/sonar\/(cas\/)?logout
 port: 8080
 principal-header: X-WEBAUTH-USER
 role-header: X-WEBAUTH-ROLE
 mail-header: X-WEBAUTH-EMAIL
 name-header: X-WEBAUTH-NAME
-create-user-endpoint: http://localhost:3000/grafana/api/admin/users
-create-group-endpoint: http://localhost:3000/grafana/api/teams
-get-user-groups-endpoint: http://localhost:3000/grafana/api/users/%v/teams
-get-user-endpoint: http://localhost:3000/grafana/api/users/lookup?loginOrEmail=%s
-remove-user-from-group-endpoint: http://localhost:3000/grafana/api/teams/%v/members/%v
-add-user-to-group-endpoint: http://localhost:3000/grafana/api/teams/%v/members
-search-team-by-name-endpoint: http://localhost:3000/grafana/api/teams/search?name=%s
-set-organization-role-endpoint: http://localhost:3000/grafana/api/orgs/1/users/%v
+create-user-endpoint: http://localhost:3000/sonar/api/admin/users
+create-group-endpoint: http://localhost:3000/sonar/api/teams
+get-user-groups-endpoint: http://localhost:3000/sonar/api/users/%v/teams
+get-user-endpoint: http://localhost:3000/sonar/api/users/lookup?loginOrEmail=%s
+remove-user-from-group-endpoint: http://localhost:3000/sonar/api/teams/%v/members/%v
+add-user-to-group-endpoint: http://localhost:3000/sonar/api/teams/%v/members
+search-team-by-name-endpoint: http://localhost:3000/sonar/api/teams/search?name=%s
+set-organization-role-endpoint: http://localhost:3000/sonar/api/orgs/1/users/%v
 ces-admin-group: cesAdmin
-grafana-admin-group: admin
-grafana-writer-group: writer
-grafana-reader-group: reader
+sonar-admin-group: admin
+sonar-writer-group: writer
+sonar-reader-group: reader
 log-format: "%{time:2006-01-02 15:04:05.000-0700} %{level:.4s} [%{module}:%{shortfile}] %{message}"
 application-exec-command: "exit 0"
-carp-resource-path: /grafana/carp-static
+carp-resource-path: /sonar/carp-static
 `
 
 const invalidType = templateConfig + `
@@ -112,8 +112,8 @@ func checkConfig(t *testing.T, config Configuration) {
 	t.Helper()
 	assert.Equal(t, "https://localhost:8080", config.BaseUrl)
 	assert.Equal(t, "https://192.168.56.2/cas", config.CasUrl)
-	assert.Equal(t, "https://localhost:8080/grafana/login", config.ServiceUrl)
-	assert.Equal(t, "\\/grafana\\/(cas\\/)?logout", config.LogoutPath)
+	assert.Equal(t, "https://localhost:8080/sonar/login", config.ServiceUrl)
+	assert.Equal(t, "\\/sonar\\/(cas\\/)?logout", config.LogoutPath)
 	assert.Equal(t, true, config.SkipSSLVerification)
 	assert.Equal(t, 8080, config.Port)
 	assert.Equal(t, "X-WEBAUTH-USER", config.PrincipalHeader)
@@ -123,5 +123,5 @@ func checkConfig(t *testing.T, config Configuration) {
 	assert.Equal(t, "%{time:2006-01-02 15:04:05.000-0700} %{level:.4s} [%{module}:%{shortfile}] %{message}", config.LoggingFormat)
 	assert.Equal(t, "DEBUG", config.LogLevel)
 	assert.Equal(t, "exit 0", config.ApplicationExecCommand)
-	assert.Equal(t, "/grafana/carp-static", config.CarpResourcePath)
+	assert.Equal(t, "/sonar/carp-static", config.CarpResourcePath)
 }
