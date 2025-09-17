867b563d3d0bd280c878d50ae22a221145582c96
Dennis Schwarzer <dennis.schwarzer@cloudogu.com>
2025-09-17 14:32:47 +0200

    #138 cleanup

--- integrationTests/cypress/support/step_definitions/required_steps_for_dogu_lib.js
+++ integrationTests/cypress/support/step_definitions/required_steps_for_dogu_lib.js
@@ -7,32 +7,19 @@ doguTestLibrary.registerSteps()
 
 
 When(/^the user clicks the dogu logout button$/, function () {
-    // cy.visit("/" + env.GetDoguName(), { failOnStatusCode: false })
-    // // wait until the logoutMenuHandler is injected
-    // // see https://github.com/cloudogu/sonar-cas-plugin/blob/develop/src/main/java/org/sonar/plugins/cas/logout/CasSonarSignOutInjectorFilter.java#L132
-    // cy.wait(1000)
-    // // Click user menu button
-    // cy.get('#userAccountMenuDropdown-trigger').scrollIntoView().click();
-    // // Click logout button
-    // cy.contains("Log out").click();
-
-
-
   const dogu = env.GetDoguName();
   const triggerSel = '#userAccountMenuDropdown-trigger[aria-haspopup="menu"]';
   const menuSel = '#userAccountMenuDropdown-dropdown';
 
   // Go to the dogu root (CAS plugin injects logout handler there)
   cy.visit(`/${dogu}`, { failOnStatusCode: false });
-  cy.screenshot('click-logout-before', { capture: 'viewport' });
   cy.closeDialogs();
   // Wait for the account button to exist instead of a blind sleep
-  cy.screenshot('click-logout-dialog', { capture: 'viewport' });
+  cy.screenshot('click-logout-dialogsclosed', { capture: 'viewport' });
 
   cy.get(triggerSel, { timeout: 15000 }).should('be.visible');
   // Open the dropdown (click twice is harmless with Radix)
   cy.get(triggerSel).click({ force: true });
-  cy.screenshot('click-logout-trigger', { capture: 'viewport' });
 
   // Wait for Radix menu to be open & visible
   cy.get(menuSel, { timeout: 10000 })
@@ -46,7 +33,7 @@ When(/^the user clicks the dogu logout button$/, function () {
       .click({ force: true });
   });
 
-  cy.screenshot('click-logout-menu', { capture: 'viewport' });
+  cy.screenshot('click-logout-success', { capture: 'viewport' });
 
   // Clean up any leftovers
   cy.clearCookies();
