8018bca74a9e35b22927f517f6d763e16c51bc8e
Florian Hübner <florian.huebner@cloudogu.com>
2025-11-18 15:43:24 +0100

    [#149] Update Makefiles

--- build/make/static-analysis.mk
+++ build/make/static-analysis.mk
@@ -2,12 +2,12 @@
 
 STATIC_ANALYSIS_DIR=$(TARGET_DIR)/static-analysis
 GOIMAGE?=golang
-GOTAG?=1.24
+GOTAG?=1.25
 CUSTOM_GO_MOUNT?=-v /tmp:/tmp
 
 REVIEW_DOG=$(TMP_DIR)/bin/reviewdog
 LINT=$(TMP_DIR)/bin/golangci-lint
-LINT_VERSION?=v2.1.6
+LINT_VERSION?=v2.5.0
 # ignore tests and mocks
 LINTFLAGS=--tests=false --timeout 10m --issues-exit-code 0
 ADDITIONAL_LINTER=-E bodyclose -E containedctx -E contextcheck -E decorder -E dupl -E errname -E forcetypeassert -E funlen -E unparam
